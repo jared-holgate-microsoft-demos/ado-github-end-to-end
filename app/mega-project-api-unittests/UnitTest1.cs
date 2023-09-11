2d2c5958130a137b626193db4626951371b6ef02
Jared Holgate <jaredholgate@microsoft.com>
2023-09-11 10:07:06 +0100

    Add working test

--- app/mega-project-api-unittests/UnitTest1.cs
+++ app/mega-project-api-unittests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using mega_project_api;
+
 namespace mega_project_api_unittests;
 
 [TestClass]
@@ -6,5 +8,7 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
+        var example = new ExampleClass();
+        Assert.AreEqual("Hello World! 2", example.ExampleMethod());
     }
 }
